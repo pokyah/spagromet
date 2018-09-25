e53cbf23fad8aac51d6978c4d5644f50fbcf05a0
pokyah <thomas.goossens@protonmail.com>
2018-09-25 13:41:01 +0000

    correcting error in RScript for bash

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ from R :
 once the package is installed, you will need to copy/paste the `bash_spatialize.R` script in the directory of your choice. This script basically parse the arguments you will pass to your bash command and send them to the `spagromet::spatialize` function.
 
 To call this script from a bash terminal :
-`a=$( Rscript --vanilla <PATH_TO_bash_spatialize.R> 2017-03-04 tsa)`
+`a=$( Rscript --vanilla <PATH_TO_bash_spatialize.R> 2017-03-04 tsa spagromet_token)`
 
 Then to echo the result in your terminal: 
 `echo"$a"`
--- bash_spatialize.R
+++ bash_spatialize.R
@@ -6,8 +6,8 @@ args = commandArgs(trailingOnly=TRUE)
 # test if there required argument are passed. if not, return an error
 if (length(args) == 0) {
   stop("At least one argument must be supplied (input file).\n", call.=FALSE)
-} else if (length(args) == 2) {
+} else if (length(args) == 3) {
   # default output file
-json = spagromet::spatialize(as.character(args[1]), as.character(args[2]))
+json = spagromet::spatialize(isodatetime = as.character(args[1]), sensor = as.character(args[2]), token_env_var = as.character(args[3]))
  cat(json)
 }
